80d690d5f752547047edc38c4305cc1a01a804a1
Sai Sree <saisree.edara@philips.com>
2018-05-11 20:06:24 +0200

    Make a working hello world page

--- src/Controller/VideoCategoryController.php
+++ src/Controller/VideoCategoryController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;   
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 /**
  * Created by Sai Sree.
  * Date: 5/11/18
  * Time: 4:12 PM
  */
-class VideoCategoryController
+class VideoCategoryController extends Controller
 {
     /**
      * @Route("/addCategory")
