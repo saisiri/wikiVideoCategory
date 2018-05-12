b3f815b80e8522ca6abb89c49b8de48d45c70131
Sai Sree <saisree.edara@philips.com>
2018-05-12 14:39:36 +0200

    Clean up skeleton code

--- src/Migrations/Version20180512123458.php
+++ src/Migrations/Version20180512123458.php
@@ -8,14 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180512121924 extends AbstractMigration
+final class Version20180512123458 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE channel_patterns (id INT AUTO_INCREMENT NOT NULL, needle VARCHAR(220) DEFAULT NULL, pattern VARCHAR(220) DEFAULT NULL, channel_id VARCHAR(220) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE video_category');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +23,6 @@ final class Version20180512121924 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE channel_patterns');
+        $this->addSql('CREATE TABLE video_category (id INT AUTO_INCREMENT NOT NULL, category VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 }
