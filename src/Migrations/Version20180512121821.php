ec3f373d8e3cf97ae0a7c58ca4f8d1c277805637
Sai Sree <saisree.edara@philips.com>
2018-05-12 15:09:58 +0200

    Create table if not exits

--- src/Migrations/Version20180512121821.php
+++ src/Migrations/Version20180512121821.php
@@ -15,7 +15,7 @@ final class Version20180512121821 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE channel_patterns (id INT AUTO_INCREMENT NOT NULL, needle VARCHAR(220) DEFAULT NULL, pattern VARCHAR(220) DEFAULT NULL, channel_id VARCHAR(220) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE IF NOT EXISTS channel_patterns (id INT AUTO_INCREMENT NOT NULL, needle VARCHAR(220) DEFAULT NULL, pattern VARCHAR(220) DEFAULT NULL, channel_id VARCHAR(220) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
